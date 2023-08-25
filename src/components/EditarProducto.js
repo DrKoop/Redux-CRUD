14827119152774d9f2333cce5a3e67352906f28a
drkoop <dr.koop.development@gmail.com>
2023-08-25 17:33:29 -0600

    viernes

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditarProducto = () => {
+  return (
+    <div> desde EditarProducto</div>
+  )
+}
+
+export default EditarProducto
---
+++ src/components/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => {
+  return (
+    <div>desde Productos</div>
+  )
+}
+
+export default Productos
